0dfc72d64a1ba620263b4bf37c38dec5bf879896
Nils Jacobsen <nilsedgarjacobsen@gmail.com>
2020-12-12 08:57:04 +0100

    Large commit. Deactivated all classes from previous version and moved Application-class to testpackage. Followed example from 'Creating a blog'-youtube tutorial.

--- src/main/java/testpackage/SpringvueprojectApplication.java
+++ src/main/java/testpackage/SpringvueprojectApplication.java
@@ -1,4 +1,4 @@
-package com.example.springvueproject;
+package testpackage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
